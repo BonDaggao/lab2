605ce20bba03f5909ad9fc04166ce6719ffc1dbd
Bon Daggao <Gryanbondaggao@gmail.com>
2023-02-07 13:24:26 +0800

    connect again

--- week9/connect.php
+++ week9/connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "apcwebprog.csf.ph";
 $username = "webprogmi211";
-$password = "webprogmi211";
+$password = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
