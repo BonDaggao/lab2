b9e0d1f8dbbd859b1870c83206896ce793181f1a
Bon Daggao <Gryanbondaggao@gmail.com>
2023-02-07 17:10:32 +0800

    table

---
+++ week9/table.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "webprogmi211";
+$password = "webprogmi211";
+$dbname = "webprogmi211";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE bsdaggao_MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+fullname VARCHAR(50) NOT NULL,
+email VARCHAR(50) NOT NULL,
+website VARCHAR(50),
+comment VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
