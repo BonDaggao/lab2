ac8e9c48807f2eb4882c2981b345cb1471106ce6
Bon Daggao <Gryanbondaggao@gmail.com>
2023-02-08 21:49:17 +0800

    guests

--- week9/guests.php
+++ week9/guests.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, fullname, email, website FROM bsdaggao_MyGuests";
+$sql = "SELECT id, fullname, email, website, comment FROM bsdaggao_MyGuests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
